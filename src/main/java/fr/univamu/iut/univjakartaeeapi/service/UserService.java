e4bded47aa473dd7fe76d1256e7e813ef5cae38b
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-04-02 21:21:10 +0200

    feat: add UserService

---
+++ src/main/java/fr/univamu/iut/univjakartaeeapi/service/UserService.java
@@ -0,0 +1,28 @@
+package fr.univamu.iut.univjakartaeeapi.service;
+
+import com.google.gson.Gson;
+import fr.univamu.iut.univjakartaeeapi.model.User;
+import fr.univamu.iut.univjakartaeeapi.repository.UserRepositoryInterface;
+
+public class UserService {
+    protected UserRepositoryInterface userRepo;
+    private Gson gson;
+
+    public UserService(UserRepositoryInterface userRepo) {
+        this.userRepo = userRepo;
+        this.gson = new Gson();
+    }
+
+    public String getAllUsersJSON() {
+        return gson.toJson(userRepo.getAllUsers());
+    }
+
+    public String getUserJSON(String id) {
+        User user = userRepo.getUser(id);
+        return user == null ? null : gson.toJson(user);
+    }
+
+    public boolean addUser(String username, String password) {
+        return userRepo.addUser(username, password);
+    }
+}
