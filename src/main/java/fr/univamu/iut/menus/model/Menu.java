0df4f9ef2eef27d6ad2782bcf7d37c880293fe6e
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-04-02 23:51:39 +0200

    Update Menu model

--- src/main/java/fr/univamu/iut/menus/model/Menu.java
+++ src/main/java/fr/univamu/iut/menus/model/Menu.java
@@ -1,18 +1,23 @@
 package fr.univamu.iut.menus.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Menu {
     protected String id;
     protected User user;
     protected String creationDate;
     protected String updateDate;
+    protected ArrayList<Dish> dishes;
 
     public Menu() {}
 
-    public Menu(String id, User user, String creationDate, String updateDate) {
+    public Menu(String id, User user, String creationDate, String updateDate, ArrayList<Dish> dishes) {
         this.id = id;
         this.user = user;
         this.creationDate = creationDate;
         this.updateDate = updateDate;
+        this.dishes = new ArrayList<Dish>();
     }
 
     public String getId() {
@@ -47,6 +52,22 @@ public class Menu {
         this.updateDate = updateDate;
     }
 
+    public ArrayList<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(ArrayList<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public void addDish(Dish dish) {
+        this.dishes.add(dish);
+    }
+
+    public void removeDish(Dish dish) {
+        this.dishes.remove(dish);
+    }
+
     @Override
     public String toString() {
         return "Menu{" +
@@ -54,6 +75,7 @@ public class Menu {
                 ", user=" + user +
                 ", creationDate='" + creationDate + '\'' +
                 ", updateDate='" + updateDate + '\'' +
+                ", dishes=" + dishes +
                 '}';
     }
 }
