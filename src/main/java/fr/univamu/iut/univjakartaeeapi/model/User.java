74f0e5f3fd691ea9b651a86b3b986a8d38925973
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-04-02 19:52:38 +0200

    feat: add User model with UserRole enum

---
+++ src/main/java/fr/univamu/iut/univjakartaeeapi/model/User.java
@@ -0,0 +1,51 @@
+package fr.univamu.iut.univjakartaeeapi.model;
+
+public class User {
+    protected String id;
+    protected String username;
+    protected String password;
+    protected UserRole role;
+
+    public User() {}
+
+    public User(String id, String username, String password, UserRole role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
