de60818c355389feab103de976fe2eba8f186f55
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-04-02 21:01:55 +0200

    Update DishRepositoryMongodb

--- src/main/java/fr/univamu/iut/univjakartaeeapi/repository/DishRepositoryMongodb.java
+++ src/main/java/fr/univamu/iut/univjakartaeeapi/repository/DishRepositoryMongodb.java
@@ -7,6 +7,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import fr.univamu.iut.univjakartaeeapi.model.Dish;
+import org.bson.types.ObjectId;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -38,7 +39,7 @@ public class DishRepositoryMongodb implements DishRepositoryInterface {
 
     @Override
     public Dish getDish(String id) {
-        Document doc = collection.find(Filters.eq("_id", id)).first();
+        Document doc = collection.find(Filters.eq("_id", new ObjectId(id))).first();
         return doc == null ? null : dishFromDocument(doc);
     }
 
@@ -54,10 +55,11 @@ public class DishRepositoryMongodb implements DishRepositoryInterface {
 
     @Override
     public boolean updateDish(String id, String name, String description, double price) {
-        Document doc = new Document("name", name)
-            .append("description", description)
-            .append("price", price);
-        return collection.replaceOne(Filters.eq("_id", id), doc).wasAcknowledged();
+        Document doc = new Document("_id", new ObjectId(id))
+                .append("name", name)
+                .append("description", description)
+                .append("price", price);
+        return collection.replaceOne(Filters.eq("_id", new ObjectId(id)), doc).wasAcknowledged();
     }
 
     @Override
@@ -70,10 +72,10 @@ public class DishRepositoryMongodb implements DishRepositoryInterface {
 
     @Override
     public boolean deleteDish(String id) {
-        return collection.deleteOne(Filters.eq("_id", id)).wasAcknowledged();
+        return collection.deleteOne(Filters.eq("_id", new ObjectId(id))).wasAcknowledged();
     }
 
     private Dish dishFromDocument(Document doc) {
-        return new Dish(doc.getString("_id"), doc.getString("name"), doc.getString("description"), doc.getDouble("price"));
+        return new Dish(doc.getObjectId("_id").toString(), doc.getString("name"), doc.getString("description"), doc.getDouble("price"));
     }
 }
