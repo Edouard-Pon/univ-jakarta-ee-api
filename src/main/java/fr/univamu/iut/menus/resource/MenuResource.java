485075b7c185c3369895e9c6549d332fb0106a55
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-04-04 23:44:13 +0200

    feat: add menu resource and service

--- src/main/java/fr/univamu/iut/menus/resource/MenuResource.java
+++ src/main/java/fr/univamu/iut/menus/resource/MenuResource.java
@@ -1,4 +1,67 @@
 package fr.univamu.iut.menus.resource;
 
+import fr.univamu.iut.menus.repository.MenuRepositoryInterface;
+import fr.univamu.iut.menus.service.MenuService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+
+@Path("/menus")
+@ApplicationScoped
 public class MenuResource {
+    private MenuService service;
+
+    public MenuResource() {}
+
+    public @Inject MenuResource(MenuRepositoryInterface menuRepo) {
+        this.service = new MenuService(menuRepo);
+    }
+
+    public MenuResource(MenuService service) {
+        this.service = service;
+    }
+
+    @GET
+    @Produces("application/json")
+    public String getAllMenus() {
+        return service.getAllMenusJSON();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public String getMenu(@PathParam("id") String id) {
+        String result = service.getMenuJSON(id);
+        if (result == null) throw new NotFoundException();
+        return result;
+    }
+
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    public Response addMenu(@FormParam("name") String name, @FormParam("description") String description, @FormParam("price") double price) {
+        if (service.addMenu(name, description, price))
+            return Response.ok("added").build();
+        else
+            return Response.status(Response.Status.CONFLICT).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes("application/x-www-form-urlencoded") // TODO - call users api to check access rights
+    public Response updateMenu(@PathParam("id") String id, @FormParam("name") String name, @FormParam("description") String description, @FormParam("price") double price) {
+        if (service.updateMenu(id, name, description, price))
+            return Response.ok("updated").build();
+        else
+            return Response.status(Response.Status.CONFLICT).build();
+    }
+
+    @DELETE
+    @Path("{id}") // TODO - call users api to check access rights
+    public Response deleteMenu(@PathParam("id") String id) {
+        if (service.deleteMenu(id))
+            return Response.ok("deleted").build();
+        else
+            return Response.status(Response.Status.CONFLICT).build();
+    }
 }
--- src/main/java/fr/univamu/iut/menus/service/MenuService.java
+++ src/main/java/fr/univamu/iut/menus/service/MenuService.java
@@ -1,4 +1,36 @@
 package fr.univamu.iut.menus.service;
 
+import com.google.gson.Gson;
+import fr.univamu.iut.menus.model.Menu;
+import fr.univamu.iut.menus.repository.MenuRepositoryInterface;
+
 public class MenuService {
+    protected MenuRepositoryInterface menuRepo;
+    private Gson gson;
+
+    public MenuService(MenuRepositoryInterface menuRepo) {
+        this.menuRepo = menuRepo;
+        this.gson = new Gson();
+    }
+
+    public String getAllMenusJSON() {
+        return gson.toJson(menuRepo.getAllMenus());
+    }
+
+    public String getMenuJSON(String id) {
+        Menu menu = menuRepo.getMenu(id);
+        return menu == null ? null : gson.toJson(menu);
+    }
+
+    public boolean addMenu(String name, String description, double price) {
+        return menuRepo.addMenu(name);
+    }
+
+    public boolean updateMenu(String id, String name, String description, double price) {
+        return menuRepo.updateMenu(id, name, description, price);
+    }
+
+    public boolean deleteMenu(String id) {
+        return menuRepo.deleteMenu(id);
+    }
 }
