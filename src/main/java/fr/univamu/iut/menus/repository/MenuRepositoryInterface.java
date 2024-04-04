c5a4de5945ba53a3a12ce42b93f3a431ee7a1d37
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-04-04 23:44:02 +0200

    feat: add menu repository interface

--- src/main/java/fr/univamu/iut/menus/repository/MenuRepositoryInterface.java
+++ src/main/java/fr/univamu/iut/menus/repository/MenuRepositoryInterface.java
@@ -1,4 +1,15 @@
 package fr.univamu.iut.menus.repository;
 
+import fr.univamu.iut.menus.model.Menu;
+import fr.univamu.iut.menus.model.User;
+
+import java.util.ArrayList;
+
 public interface MenuRepositoryInterface {
+    public void close();
+    public Menu getMenu(String id);
+    public ArrayList<Menu> getAllMenus();
+    public boolean addMenu(String userId);
+    public boolean deleteMenu(String id);
+    public boolean updateMenu(String id, String name, String description, double price);
 }
